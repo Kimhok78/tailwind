f8de4182f50c5fdd05ab23c1db5491ad2a520f51
Kimhok78 <lengmuykea1112@gmail.com>
2025-07-25 14:51:03 +0700

    message

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Navbar from './components/navbar'
 import Footer from './components/footer'
+import Home from './components/Home'
 
 const App = () => {
   return (
     <>
     <nav><Navbar/></nav>
   
-    <main></main>
+    <main>
+      <Home/>
+    </main>
 
     <footer>
       <Footer/>
